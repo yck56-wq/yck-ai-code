090368cfe29b307ad420948fb8b590a18a3b92cc
yck <yu13847652401@163.com>
2026-02-15 15:55:22 +0800

    初始化Spring Boot项目，添加基础配置和通用组件

--- src/main/java/com/yck/aicode/YckAiCodeApplication.java
+++ src/main/java/com/yck/aicode/YckAiCodeApplication.java
@@ -1,9 +1,11 @@
 package com.yck.aicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yck.aicode.mapper")
 public class YckAiCodeApplication {
 
     public static void main(String[] args) {
