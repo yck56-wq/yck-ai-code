a513f4297cb78d288a95b631157a767ca22e187c
yck <yu13847652401@163.com>
2026-03-20 15:28:00 +0800

    系统优化 - Redis缓存优化并且把vue异步构建改为同步构建

--- src/main/java/com/yck/aicode/core/handler/JsonMessageStreamHandler.java
+++ src/main/java/com/yck/aicode/core/handler/JsonMessageStreamHandler.java
@@ -1,13 +1,11 @@
 package com.yck.aicode.core.handler;
 
-import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.yck.aicode.ai.model.message.*;
 import com.yck.aicode.ai.tools.BaseTool;
 import com.yck.aicode.ai.tools.ToolManager;
-import com.yck.aicode.constant.AppConstant;
 import com.yck.aicode.core.builder.VueProjectBuilder;
 import com.yck.aicode.model.entity.User;
 import com.yck.aicode.model.enums.ChatHistoryMessageTypeEnum;
@@ -27,9 +25,6 @@ import java.util.Set;
 @Slf4j
 @Component
 public class JsonMessageStreamHandler {
-
-    @Resource
-    private VueProjectBuilder vueProjectBuilder;
 
     @Resource
     private ToolManager toolManager;
@@ -61,9 +56,6 @@ public class JsonMessageStreamHandler {
                     // 流式响应完成后，添加 AI 消息到对话历史
                     String aiResponse = chatHistoryStringBuilder.toString();
                     chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
-                    // 异步构造 Vue 项目
-                    String projectPath = AppConstant.CODE_OUTPUT_ROOT_DIR + "/vue_project_" + appId;
-                    vueProjectBuilder.buildProjectAsync(projectPath);
                 })
                 .doOnError(error -> {
                     // 如果AI回复失败，也要记录错误消息
